4090c2cd43106e5d9f0ab2bf3ebf86333d498156
Elizabeth Lee <eclee25@gmail.com>
2025-05-09 10:10:09 -0700

    add label alerttypes

---
+++ R/label_alerttypes.R
@@ -0,0 +1,11 @@
+#' Title label_alerttypes
+#' @name label_alerttypes
+#' @description function that converts alert types to an ordered factor with pretty labels
+#' @param basedf a df with a column for alert_type
+#' @return dataframe
+#' @export
+label_alerttypes <- function(basedf){
+  
+  basedf %>%
+    dplyr::mutate(alert_type = factor(alert_type, levels = c("case", "cumsum", "rate", "trend"), labels = c("weekly case", "cum case", "rate", "trend"), ordered = TRUE))
+}
