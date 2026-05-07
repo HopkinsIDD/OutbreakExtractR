17bb04cd9000f405aa571be3e8291b6b50df7127
Qulu <44678888+QLLZ@users.noreply.github.com>
2026-05-07 11:53:24 -0400

    Update identify_epidemic_start.R

--- R/identify_epidemic_start.R
+++ R/identify_epidemic_start.R
@@ -10,7 +10,7 @@ identify_epidemic_start <- function(
     require_increasing_trend = FALSE,
     window_weeks = 3,
     cumulative_windows = 3,
-    cumulative_case_threshold_ratio = 10
+    cumulative_case_threshold_ratio = 1.5
 ) {
   outbreak_data_with_epistart<- outbreak_data
   outbreak_data_with_epistart$epidemic_start <- FALSE
@@ -46,13 +46,15 @@ identify_epidemic_start <- function(
 } else if(outbreak_start_definition=="dual_window"){
     outbreak_data_with_epistart <- outbreak_data_with_epistart %>% 
       mutate(risk_num = as.integer(risk=='high'),
-             sCh_cum_thresh = threshold*pop*cumulative_case_threshold_ratio) %>% 
+             sCh_cum_thresh = threshold*pop*cumulative_case_threshold_ratio,
+             sCh_pos = as.integer(sCh > 0)) %>% 
       mutate(
         d1 =slider::slide_dbl(risk_num,sum,.before = window_weeks-1, .complete=TRUE),
-        d2= slider::slide_dbl(sCh,sum,.before=cumulative_windows-1,.complete=TRUE)
+        d2= slider::slide_dbl(sCh,sum,.before=cumulative_windows-1,.complete=TRUE),
+        d2_2 = slider::slide_dbl(sCh_pos,sum,.before = cumulative_windows - 1,.complete = TRUE)
       ) %>% 
       mutate(
-        alert    = d1 >= min_weeks_above | (d2 >= sCh_cum_thresh & sCh_cum_thresh!=0),
+        alert = d1 >= min_weeks_above|(d2>=sCh_cum_thresh & sCh_cum_thresh!=0 & d2_2 >=2),
         d1_start = if_else(alert, row_number() - (window_weeks - 1), NA_integer_),
         d2_start = if_else(alert, row_number() - (cumulative_windows - 1), NA_integer_),
         epidemic_start = row_number() %in% c(d1_start, d2_start)
