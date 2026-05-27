22748fec8b5c96b521b3b05abf3eeedd85717a9d
Qulu <44678888+QLLZ@users.noreply.github.com>
2026-05-27 17:01:00 -0400

    Update identify_epidemic_start.R

--- R/identify_epidemic_start.R
+++ R/identify_epidemic_start.R
@@ -5,17 +5,20 @@
 #' @description per location period per start_day group: weekly cholera incidence
 identify_epidemic_start <- function(
     outbreak_data = outbreak_data, #consecutive weekly outbreak data
-    outbreak_start_definition = c("consecutive","dual_window"), 
+    outbreak_start_definition = c("consecutive","dual_window"),
     min_weeks_above = 2,
     require_increasing_trend = FALSE,
     window_weeks = 3,
+    use_cumulative_trigger=TRUE,
+    cumulative_trigger_type=c("cumulative_case_threshold","cumulative_case_threshold_and_min_cases",'cumulative_case_threshold_and_nonzero_weeks'),
     cumulative_windows = 3,
-    cumulative_case_threshold_ratio = 1.5
+    cumulative_case_threshold_ratio = 1.5,
+    cumulative_min_cases = NULL,
+    nonzero_windows =NULL
 ) {
   outbreak_data_with_epistart<- outbreak_data
   outbreak_data_with_epistart$epidemic_start <- FALSE
-  outbreak_data_with_epidemic_start<-data.frame()
-  
+
   if(outbreak_start_definition == "consecutive"){
     if(nrow(outbreak_data_with_epistart) < min_weeks_above){
       return(outbreak_data_with_epistart)
@@ -44,23 +47,49 @@ identify_epidemic_start <- function(
       }
   }
 } else if(outbreak_start_definition=="dual_window"){
-    outbreak_data_with_epistart <- outbreak_data_with_epistart %>% 
-      mutate(risk_num = as.integer(risk=='high'),
-             sCh_cum_thresh = threshold*pop*cumulative_case_threshold_ratio,
-             sCh_pos = as.integer(sCh > 0)) %>% 
-      mutate(
-        d1 =slider::slide_dbl(risk_num,sum,.before = window_weeks-1, .complete=TRUE),
-        d2= slider::slide_dbl(sCh,sum,.before=cumulative_windows-1,.complete=TRUE),
-        d2_2 = slider::slide_dbl(sCh_pos,sum,.before = cumulative_windows - 1,.complete = TRUE)
-      ) %>% 
-      mutate(
-        alert = d1 >= min_weeks_above|(d2>=sCh_cum_thresh & sCh_cum_thresh!=0 & d2_2 >=2),
-        d1_start = if_else(alert, row_number() - (window_weeks - 1), NA_integer_),
-        d2_start = if_else(alert, row_number() - (cumulative_windows - 1), NA_integer_),
-        epidemic_start = row_number() %in% c(d1_start, d2_start)
+  
+  if (sum(outbreak_data_with_epistart$risk=="high")<min_weeks_above) {
+    return(outbreak_data_with_epistart)
+  }
+  
+  outbreak_data_with_epistart <- outbreak_data_with_epistart %>%
+    mutate(
+      risk_num= as.integer(risk == 'high'),
+      sCh_cum_thresh = threshold * pop * cumulative_case_threshold_ratio,
+      sCh_pos= as.integer(sCh > 0)
+    ) %>%
+    mutate(
+      d1   = slider::slide_dbl(risk_num, sum, .before = window_weeks - 1,.complete = TRUE), # calculate the number of high risk weeks among window weeks
+      d2   = slider::slide_dbl(sCh,sum, .before = cumulative_windows - 1,.complete = TRUE),# calculate the cumulative cases over the cumulative windows
+      d2_2 = slider::slide_dbl(sCh_pos,  sum, .before = cumulative_windows - 1,.complete = TRUE) # measure the positive case weeks over the cumulative windows
+    ) %>%
+    mutate(
+      d1= replace_na(d1, 0),
+      d2 = replace_na(d2, 0),
+      d2_2= replace_na(d2_2, 0),
+      
+      d2_thresh_met = d2 >= sCh_cum_thresh & sCh_cum_thresh != 0,
+      
+      cumulative_alert = if (!use_cumulative_trigger) {
+        rep(FALSE, n())
+      } else if (cumulative_trigger_type == "cumulative_case_threshold") {
+        d2_thresh_met
+      } else if (cumulative_trigger_type == "cumulative_case_threshold_and_min_cases") {
+        d2_thresh_met & d2 >= cumulative_min_cases
+      } else if (cumulative_trigger_type == "cumulative_case_threshold_and_nonzero_weeks") {
+        d2_thresh_met & d2_2 >= nonzero_windows
+      } else {
+        rep(FALSE, n())
+      },
+      row_id = row_number(),
+      alert = (d1 >= min_weeks_above) | cumulative_alert,
+      d1_start = if_else(d1 >= min_weeks_above, row_id - (window_weeks - 1), NA_integer_),
+      d2_start = if_else(cumulative_alert, row_id - (cumulative_windows - 1), NA_integer_),
+      d1_start = if_else(d1_start %in% row_id[risk == "high"], d1_start, NA_integer_),
+      epidemic_start = row_id %in% na.omit(c(d1_start, d2_start))
+      
       )
   }
-  outbreak_data_with_epidemic_start <- rbind(outbreak_data_with_epidemic_start,
-                                             outbreak_data_with_epistart)
-  return(outbreak_data_with_epidemic_start)
+
+  return(outbreak_data_with_epistart[,colnames(outbreak_data_with_epistart)%in%c(colnames(outbreak_data),"epidemic_start")])
 }
