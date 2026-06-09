5e335bd77a7ee5bfca5df40f072ad8cbd254cc3b
javier <javier.perezsaez@gmail.comn>
2026-06-09 14:13:41 +0200

    use hack to get around ssl

--- analysis/01_pull_data.R
+++ analysis/01_pull_data.R
@@ -70,7 +70,7 @@ location_str <- make_taxdat_location(opt$who_region, opt$country_iso3)
 message("Pulling data: ", location_str,
         "  [", opt$time_lower_bound, " → ", opt$time_upper_bound, "]")
 
-raw_sf <- taxdat::pull_taxonomy_data(
+raw_sf <- pull_taxonomy_data(
   username   = api_user,
   password   = api_key,
   locations  = location_str,
